1bc6c2d99e603a1c0d506c0063bd67f637b4c716
workuy <workuy2470560615@outlook.com>
2020-12-09 21:43:21 +0800

    初始化项目

---
+++ Weibo/Weibo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Weibo
+//
+//  Created by workuy on 2020/12/9.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Weibo/Weibo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Weibo
+//
+//  Created by workuy on 2020/12/9.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
